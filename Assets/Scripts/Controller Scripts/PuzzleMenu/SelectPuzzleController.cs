99df20e67ed8d0284a99e6495c2fcaed4cd398c1
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-25 11:40:28 +0600

    programming menu

--- Assets/Scripts/Controller Scripts/PuzzleMenu/SelectPuzzleController.cs
+++ Assets/Scripts/Controller Scripts/PuzzleMenu/SelectPuzzleController.cs
@@ -2,15 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SelectPuzzleController : MonoBehaviour {
+public class SelectPuzzleController : MonoBehaviour 
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
