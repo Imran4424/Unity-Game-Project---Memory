5d3ecafd1d320936994fd81fd5e1fa5cbd6860d3
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-26 09:01:05 +0600

    level select panel

--- Assets/Scripts/Controller Scripts/PuzzleMenu/SelectLevelController.cs
+++ Assets/Scripts/Controller Scripts/PuzzleMenu/SelectLevelController.cs
@@ -22,7 +22,7 @@ public class SelectLevelController : MonoBehaviour
 		selectPuzzleMenuPanel.SetActive (true);
 
 		selectPuzzleLevelAnim.Play ("SlideOut");
-		yield return new WaitForSeconds (0.4f);
+		yield return new WaitForSeconds (0.3f);
 		selectPuzzleMenuAnim.Play ("SlideIn");
 
 		yield return new WaitForSeconds (1f);
