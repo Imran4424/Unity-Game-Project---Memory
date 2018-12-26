129b4871cac218fe2cd8918bd71dfe74edf647a4
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-26 08:44:19 +0600

    level select panel

--- Assets/Scripts/Controller Scripts/PuzzleMenu/SelectLevelController.cs
+++ Assets/Scripts/Controller Scripts/PuzzleMenu/SelectLevelController.cs
@@ -2,8 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SelectLevelController : MonoBehaviour 
+public class SelectLevelController : MonoBehaviour
 {
+	[SerializeField]
+	private GameObject selectPuzzleMenuPanel, selectPuzzleLevelPanel;
 
-	
+	[SerializeField]
+	private Animator selectPuzzleMenuAnim, selectPuzzleLevelAnim;
+
+	private string selectedLevel;
+
 }
